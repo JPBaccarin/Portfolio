c2cc116bc29d72cf575d8ce855abb76daf1e09e5
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2026-01-27 15:39:13 -0300

    Changes to be committed: 	modified:   baccarin/next.config.ts 	modified:   baccarin/package.json

--- baccarin/next.config.ts
+++ baccarin/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
-  
+  output: "export",
+  basePath: "/Portfolio", // Substitua pelo nome do seu repositório se for diferente
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
